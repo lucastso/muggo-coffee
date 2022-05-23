1f5a62862f0fab486bc061537cba34efc95dd74a
lucastso <lucastassisouza@gmail.com>
2022-05-22 22:44:17 -0300

    minor changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "55402f4d767ac8f1"; }
+/******/ 		__webpack_require__.h = function() { return "1d1e484e25453d33"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
