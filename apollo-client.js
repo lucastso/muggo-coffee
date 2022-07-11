f07b443a20c117b8f62422ff59b14bf39b414ae9
lucastso <lucastassisouza@gmail.com>
2022-07-11 17:29:19 -0300

    fix dependencies

--- apollo-client.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl5g1kdtr39ir01t7b485c4jy/master",
-  cache: new InMemoryCache(),
-});
-
-export default client;
